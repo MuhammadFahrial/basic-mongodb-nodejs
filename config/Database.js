47780dc69ce23f10c93f5d697747364af8ecaa66
MuhammadFahrial <rialjhe111@gmail.com>
2024-09-29 22:49:24 +0800

    vercel

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 
-const url =
-  "mongodb+srv://andimuhammadfahrials:hCCJTxnAcG23TUwY@cluster0.4djuz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
 const connectDB = async () => {
   await mongoose.connect(proccess.env.MONGODB_CONNECT_URI);
 };
