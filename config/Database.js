8dedbcc50c2118b5b7aaced724813c9d737b55d3
MuhammadFahrial <rialjhe111@gmail.com>
2024-09-29 22:29:52 +0800

    update

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const url = "mongodb://localhost:27017/fullstack-db";
+const url =
+  "mongodb+srv://andimuhammadfahrials:hCCJTxnAcG23TUwY@cluster0.4djuz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectDB = async () => {
   await mongoose.connect(url);
